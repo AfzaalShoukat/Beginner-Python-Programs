5af1dc564de0a94ae1e9ef75ded2f7020eab8c1c
AfzaalShoukat <afzaalshazada@gmail.com>
2024-10-27 21:21:04 +0500

    ggg

---
+++ sum_two_numbers.py
@@ -0,0 +1,17 @@
+#Solution-1 (with pre-defined Variables)
+
+num1 = 30
+num2 = 50
+
+sum = num1 + num2
+
+print("The sum of given two Number is ", sum)
+
+#Solution-2 (with user-input)
+
+num3 =float(input("Enter the frist Number:"))
+num4 =float(input("Enter the second Number:"))
+
+add = num3 +num4
+print("The sum of given two Number is ",add)
+
