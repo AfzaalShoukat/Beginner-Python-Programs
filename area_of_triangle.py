2e53ebce73caf849d067040fe21369ebcd90039f
AfzaalShoukat <afzaalshazada@gmail.com>
2024-10-27 23:43:51 +0500

    area of traingle

---
+++ area_of_triangle.py
@@ -0,0 +1,5 @@
+# To find the area of triangle
+
+length = int(input("Enter the length of triangle : "))
+width = int(input("Enter the width of triangle : "))
+print("The total area of triangle is :",length*width)
---
+++ square_root.py
@@ -0,0 +1,16 @@
+# To find the square root
+
+num = int(input("Enter the Number :"))
+
+square_root = num**0.2  # square root of the user value/num**(1/2)
+
+print("The Square root of the number is : ",square_root)
+
+# using Math Module
+
+import math
+
+num = int(input("Enter the number "))
+sr = math.sqrt(num)
+
+print("The square root of this number is  : ",sr)
