e5170588a4f5885fe57eedcf1c53b0d121fe7468
AfzaalShoukat <afzaalshazada@gmail.com>
2024-10-27 23:55:26 +0500

    swap two variable

---
+++ swap_two_variables.py
@@ -0,0 +1,12 @@
+# swap two variable x and y
+
+x = 12
+y = 13
+
+temp = x 
+print("The swap two variables is temp : ",temp)
+x = y
+print("The swap two variables is x : ",x)
+y = temp
+print("The swap two variables is y : ",y)
+
